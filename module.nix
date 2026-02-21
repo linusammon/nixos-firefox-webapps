e762a0a0ce150c3a4235f6b22517fb18a429fbc6
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-02-21 08:04:23 +0100

    fix: quote arguments in desktop entry exec string

--- module.nix
+++ module.nix
@@ -107,7 +107,7 @@ in
         name = safeName;
         inherit (app) comment;
         desktopName = app.name;
-        exec = "${pkgs.firefox}/bin/firefox --profile .mozilla/firefox-webapps/${safeName} --name ${app.name} ${app.url}";
+        exec = "${pkgs.firefox}/bin/firefox --profile .mozilla/firefox-webapps/${safeName} --name \"${app.name}\" \"${app.url}\"";
         terminal = false;
         type = "Application";
         icon = if app.icon != null then app.icon else "firefox";
